93f49a85228dad1fdce1a8aee4f89ea94647f968
Mirza-Kashan-Ahmed <mirzaka2490@gmail.com>
2025-01-18 09:07:01 +0000

    variables

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,15 @@
-console.log("Mirza");
+var accountname = "Mirza"
+let accountDetail = "kashan ahmed"
+const username = 1
+
+accountname = 3
+
+console.log(accountname);
+console.log(accountDetail);
+console.log(username);
+
+console.log(a); // undefined (due to hoisting)
+var a = 5;
+
+console.log(b); // ReferenceError: Cannot access 'b' before initialization
+let b = 10;
